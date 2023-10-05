0f93bfbd14535d27cb0a5adbdb28ef08380b188f
Pacatro <paco.algar.15@gmail.com>
2023-10-05 09:20:13 +0200

    empezando ej2

---
+++ src/factory/InscriptionFactory.java
@@ -0,0 +1,8 @@
+package factory;
+
+public abstract class InscriptionFactory {
+    /*
+    public abstract EarlyRegInscription createEarlyRegInscription();    
+    public abstract LateRegInscription createLateRegInscription();
+    */
+}

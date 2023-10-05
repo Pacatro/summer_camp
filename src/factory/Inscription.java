3390e825960f2ce57503f751c93fd0ade42378e4
mayumar <maytalbada@gmail.com>
2023-10-05 11:38:37 +0200

    Get y Set de Inscription hechos

--- src/factory/Inscription.java
+++ src/factory/Inscription.java
@@ -3,6 +3,9 @@ package factory;
 import java.time.LocalDate;
 
 public class Inscription {
+    
+    /* Atributtes */
+    
     private String id_participant;
 
     private String id_campament;
@@ -13,10 +16,51 @@ public class Inscription {
 
     private boolean cancellation;
 
+    /* Constructor */
+
     public Inscription(){
         this.id_participant = "";
         this.id_campament = "";
         this.date = LocalDate.now();
         this.price = 0f;
     }
+
+    /* Gets y Sets */
+
+    public String getIdParticipant(){
+        return this.id_participant;
+    }
+
+    public String getIdCampament(){
+        return this.id_campament;
+    }
+
+    public LocalDate getDate(){
+        return this.date;
+    }
+
+    public float getPrice(){
+        return this.price;
+    }
+
+    public boolean getCancellation(){
+        return this.cancellation;
+    }
+
+    public void setIdParticipant(String id_participant){
+        this.id_participant = id_participant;
+    }
+
+    public void setIdCampament(String id_campament){
+        this.id_campament = id_campament;
+    }
+
+    public void setDate(LocalDate date){
+        this.date = date;
+    }
+
+    public void setPrice(float price){
+        this.price = price;
+    }
+
 }

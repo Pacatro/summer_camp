c35d43c76082f64104f4284d73ccc229f055986e
Pacatro <paco.algar.15@gmail.com>
2023-10-11 22:05:26 +0200

    error en factory

--- src/classes/InscriptionsManager.java
+++ src/classes/InscriptionsManager.java
@@ -1,5 +1,26 @@
 package classes;
 
+import java.util.Scanner;
+import factory.EarlyRegInscriptionFactory;
+import factory.LateRegInscriptionFactory;
+
 public class InscriptionsManager {
+    public void enroll(Assistant assistant, Campament campament){
+        Scanner scanner = new Scanner(System.in);
+        int option;    
+        
+        System.out.println("Introduzca la modalidad de la inscripcion:");
+        System.out.println("1. Registro temprano");
+        System.out.println("1. Registro tardio");
+        option = scanner.nextInt();
+
+        if(option == 1){
+            EarlyRegInscriptionFactory earlyRegInscriptionFactory = new EarlyRegInscriptionFactory();
+        }
+
+
+
+    }
     
+
 }

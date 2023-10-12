1cdd985f8b03354b9da5f1047c3b74bfcc3685f9
Pacatro <paco.algar.15@gmail.com>
2023-10-12 16:14:52 +0200

    gestor casi acabado

--- src/classes/InscriptionsManager.java
+++ src/classes/InscriptionsManager.java
@@ -1,41 +1,70 @@
 package classes;
 
-import java.util.Scanner;
-
+import java.time.LocalDate;
+import java.util.ArrayList;
+import factory.Inscription;
 import factory.CompleteInscription;
 import factory.EarlyRegInscriptionFactory;
-import factory.Inscription;
-import factory.LateRegInscriptionFactory;
 import factory.ParcialInscription;
+import factory.InscriptionFactory;
+import factory.LateRegInscriptionFactory;
+
 
 public class InscriptionsManager {
-    public void enroll(Assistant assistant, Campament campament){
-        Scanner scanner = new Scanner(System.in);
-        EarlyRegInscriptionFactory earlyRegInscriptionFactory = new EarlyRegInscriptionFactory();
-        LateRegInscriptionFactory lateRegInscriptionFactory = new LateRegInscriptionFactory();
-        Inscription inscription = null;
+    private ArrayList<Monitor> monitors;
 
-        System.out.println("Introduzca la modalidad de la inscripcion:");
-        System.out.println("1. Registro temprano");
-        System.out.println("2. Registro tardío");
-        int regOption = scanner.nextInt();
+    public InscriptionsManager(){}
 
-        System.out.println("Introduzca el tipo de inscripcion:");
-        System.out.println("1. Inscripcion completa");
-        System.out.println("2. Inscripcion parcial");
-        int typeOption = scanner.nextInt();
+    public InscriptionFactory getFactory(Campament campament){
+        LocalDate date = LocalDate.now();
+        LocalDate campamentDate = campament.getInitDate();
+        
+        int dif = date.compareTo(campamentDate);
 
-        if (regOption == 1 && typeOption == 1) {
-            inscription = earlyRegInscriptionFactory.createCompleteInscription();
-        } else if (regOption == 1 && typeOption == 2) {
-            inscription = earlyRegInscriptionFactory.createParcialInscription();
-        } else if (regOption == 2 && typeOption == 1) {
-            inscription = lateRegInscriptionFactory.createCompleteInscription();
-        } else if (regOption == 2 && typeOption == 2) {
-            inscription = lateRegInscriptionFactory.createParcialInscription();
+        if(dif >= 15)
+            return new EarlyRegInscriptionFactory();
+        else if(dif >= 2)
+            return new LateRegInscriptionFactory();
+        
+        return null;
+    }
+
+    public double calcPrice(Campament campament){
+        double price = 300.0;
+        ArrayList<Activity> activities = campament.getActivities();
+
+        if(getFactory(campament).getClass().getSimpleName() == "EarlyRegInscriptionFactory")
+            price -= 100.0;
+        
+        for(int i = 0; i < activities.size(); i++)
+            price += 30.0;
+
+        return price;    
+    }
+
+    public void enroll(Campament campament, Assistant assistant, Schendule schendule, 
+                       String type, ArrayList<Inscription> inscriptions){
+
+        Inscription inscription;
+        InscriptionFactory factory = getFactory(campament);
+
+        if(factory == null)
+            return;
+
+        if(assistant.getAtention()){
+            for(Monitor m : this.monitors){
+                if(m.isEspecial())
+                    campament.associateSpecialMonitor(m);
+            }
         }
 
-        System.out.println(inscription);
-    }    
+        if(type == "parcial")
+            inscription = factory.createParcialInscription(campament, assistant);
+        else
+            inscription = factory.createCompleteInscription(campament, assistant, schendule);
 
+        double price = calcPrice(campament);
+        inscription.setPrice(price);
+        inscriptions.add(inscription);
+    }
 }

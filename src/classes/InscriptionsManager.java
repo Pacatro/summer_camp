147c413980b36c7445b2b5dfcdcf3b43f7371a63
Pacatro <paco.algar.15@gmail.com>
2023-10-12 17:34:44 +0200

    gestor de inscripciones hecho (creo)

--- src/classes/InscriptionsManager.java
+++ src/classes/InscriptionsManager.java
@@ -8,8 +8,6 @@ import factory.InscriptionFactory;
 import factory.LateRegInscriptionFactory;
 
 public class InscriptionsManager {
-    private ArrayList<Monitor> monitors;
-
     public InscriptionsManager(){}
 
     public InscriptionFactory getFactory(Campament campament){
@@ -26,41 +24,53 @@ public class InscriptionsManager {
         return null;
     }
 
-    public double calcPrice(Campament campament){
+    public double calcPrice(Campament campament, boolean isEspecial){
         double price = 300.0;
         ArrayList<Activity> activities = campament.getActivities();
 
         if(getFactory(campament).getClass().getSimpleName() == "EarlyRegInscriptionFactory")
             price -= 100.0;
         
-        for(int i = 0; i < activities.size(); i++)
-            price += 30.0;
+        if(!isEspecial){
+            for(int i = 0; i < activities.size(); i++)
+                price += 20.0;
+        }
 
         return price;    
+    }
+
+    public boolean canEnroll(Campament campament){
+        LocalDate date = LocalDate.now();
+        LocalDate campamentDate = campament.getInitDate();
+
+        return date.isBefore(campamentDate);
     }
 
     public void enroll(Campament campament, Assistant assistant, Schendule schendule, 
                        String type, ArrayList<Inscription> inscriptions){
 
         Inscription inscription;
+        ArrayList<Monitor> monitors = campament.getMonitors();
         InscriptionFactory factory = getFactory(campament);
 
         if(factory == null)
             return;
 
         if(assistant.getAtention()){
-            for(Monitor m : this.monitors){
+            for(Monitor m : monitors){
                 if(m.isEspecial())
                     campament.associateSpecialMonitor(m);
             }
         }
+
+        if(!canEnroll(campament)) return;
 
         if(type == "parcial")
             inscription = factory.createParcialInscription(campament, assistant);
         else
             inscription = factory.createCompleteInscription(campament, assistant, schendule);
 
-        double price = calcPrice(campament);
+        double price = calcPrice(campament, assistant.getAtention());
         inscription.setPrice(price);
         inscriptions.add(inscription);
     }

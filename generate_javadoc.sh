989ee553b960fca5d53a23e3ed6959ec79b9ba42
mayumar <maytalbada@gmail.com>
2023-11-12 16:53:09 +0100

    Javadoc v2 generado

---
+++ generate_javadoc.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+javadoc -d docs/ src/es/uco/pw/display/*.java src/es/uco/pw/display/exceptions/*.java src/es/uco/pw/data/dao/*/*.java src/es/uco/pw/data/common/*.java src/es/uco/pw/business/*/*.java src/es/uco/pw/business/common/*/*.java

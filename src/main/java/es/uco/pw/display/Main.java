4a597e452e1703bbe4c72d0eccfd33320454115b
mayumar <maytalbada@gmail.com>
2023-11-22 09:00:30 +0100

    TENEMOS BASE DE DATOS, GENTE

--- src/main/java/es/uco/pw/display/Main.java
+++ src/main/java/es/uco/pw/display/Main.java
@@ -2,24 +2,13 @@ package es.uco.pw.display;
 
 import java.util.Scanner;
 
-import es.uco.pw.business.common.userType.UserType;
-import es.uco.pw.business.user.UserDTO;
-import es.uco.pw.data.dao.user.UserDAO;
-
-
 public class Main {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
 
         Menu menu = new Menu(scanner);
 
-        //menu.mainMenu();
-
-        UserDTO user = new UserDTO("juan@example.com", "Juan", "password789", UserType.ASSISTANT);
-
-        UserDAO dao = new UserDAO();
-
-        dao.insert(user);
+        menu.mainMenu();
     }
 
 }

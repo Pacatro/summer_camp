a79f3a3a3aa3ef341b2440825af3aa49dfd792fb
Pacatro <paco.algar.15@gmail.com>
2023-12-15 01:20:40 +0100

    servlets casi completos

--- src/main/java/es/uco/pw/business/servlets/CompleteInscriptionsServlet.java
+++ src/main/java/es/uco/pw/business/servlets/CompleteInscriptionsServlet.java
@@ -1,10 +1,12 @@
 package es.uco.pw.business.servlets;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 
 import es.uco.pw.business.assistant.AssistantDTO;
 import es.uco.pw.business.campament.CampamentDTO;
+import es.uco.pw.business.common.schedule.Schedule;
 import es.uco.pw.business.managers.AssistantManager;
 import es.uco.pw.business.managers.CampamentsManager;
 import es.uco.pw.business.managers.InscriptionsManager;
@@ -18,15 +20,64 @@ import java.util.Properties;
  * Servlet implementation for handling complete camp inscriptions cancellation.
  * This servlet is mapped to the URL pattern "/completeInscription".
  *
+ * The expected parameters for the POST request are "camp-id", "assis-id" and "schedule".
+ * These parameters are used to identify the camp, assistant and schedule for create the complete inscription.
+ *
  * The expected parameters for the DELETE request are "camp-id" and "assis-id".
  * These parameters are used to identify the camp and assistant for which the complete inscription should be canceled.
- *
+ * 
  * The authentication is performed using the "customerBean" attribute stored in the session,
  * ensuring that the user is authorized to perform the cancellation operation.
  *
  */
 @WebServlet(name = "completeInscriptionsServlet", urlPatterns = "/completeInscription")
 public class CompleteInscriptionsServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+
+        CustomerBean customerBean = (CustomerBean) session.getAttribute("customerBean");
+
+        if(customerBean == null) {
+            res.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized: The user is not an admin");
+            return;
+        }
+
+        if(req.getParameter("assis-id") == null || 
+           req.getParameter("camp-id") == null || 
+           req.getParameter("schedule") == null) {
+            res.sendError(HttpServletResponse.SC_BAD_REQUEST, "Bad Request: Missing parameters");
+            return;
+        }
+        
+        // Estos parametros se pasaraian de la siguiente forma -> /summer_camp/completeInscription?camp-id=1&assis-id=2
+        int campId = Integer.parseInt(req.getParameter("camp-id"));
+        int assisId = Integer.parseInt(req.getParameter("assis-id"));
+        Schedule schedule = Schedule.valueOf(req.getParameter("schedule"));
+
+        try {
+            // TODO: MOSTRAR AVISOS
+            Properties configProperties = new Properties();
+            Properties sqlProperties = new Properties();
+            sqlProperties.load(getServletContext().getResourceAsStream("/WEB-INF/sql.properties"));
+            configProperties.load(getServletContext().getResourceAsStream("/WEB-INF/config.properties"));
+
+            CampamentsManager campamentsManager = new CampamentsManager(sqlProperties, configProperties);
+            AssistantManager assistantManager = new AssistantManager(sqlProperties, configProperties);
+            InscriptionsManager inscriptionsManager = new InscriptionsManager(sqlProperties, configProperties);
+
+            CampamentDTO campamentDTO = campamentsManager.getById(campId);
+            AssistantDTO assistantDTO = assistantManager.getById(assisId);
+
+            inscriptionsManager.enrollComplete(campamentDTO, assistantDTO, schedule);
+            
+            res.setStatus(HttpServletResponse.SC_CREATED);
+        } catch (Exception e) {
+            e.getStackTrace();
+            res.sendError(HttpServletResponse.SC_BAD_REQUEST, "Bad Request: " + e.getMessage());
+        }
+    }
+    
     @Override
     protected void doDelete(HttpServletRequest req, HttpServletResponse res) throws FileNotFoundException, IOException {
         HttpSession session = req.getSession();
@@ -57,11 +108,9 @@ public class CompleteInscriptionsServlet extends HttpServlet {
             AssistantManager assistantManager = new AssistantManager(sqlProperties, configProperties);
             InscriptionsManager inscriptionsManager = new InscriptionsManager(sqlProperties, configProperties);
 
-            CampamentDTO campamentDTO = null;
-            AssistantDTO assistantDTO = null;
+            CampamentDTO campamentDTO = campamentsManager.getById(campId);
+            AssistantDTO assistantDTO = assistantManager.getById(assisId);
 
-            campamentDTO = campamentsManager.getById(campId);
-            assistantDTO = assistantManager.getById(assisId);
             inscriptionsManager.cancelComplete(campamentDTO, assistantDTO);
             
             res.setStatus(HttpServletResponse.SC_OK);
--- src/main/java/es/uco/pw/business/servlets/ParcialInscriptionsServlet.java
+++ src/main/java/es/uco/pw/business/servlets/ParcialInscriptionsServlet.java
@@ -18,6 +18,9 @@ import java.util.Properties;
  * Servlet implementation for handling partial camp inscriptions cancellation.
  * This servlet is mapped to the URL pattern "/parcialInscription".
  *
+ * The expected parameters for the POST request are "camp-id" and "assis-id".
+ * These parameters are used to identify the camp and assistant for create the parcial inscription.
+ * 
  * The expected parameters for the DELETE request are "camp-id" and "assis-id".
  * These parameters are used to identify the camp and assistant for which the partial inscription should be canceled.
  *
@@ -27,6 +30,49 @@ import java.util.Properties;
  */
 @WebServlet(name = "parcialInscriptionsServlet", urlPatterns = "/parcialInscription")
 public class ParcialInscriptionsServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+
+        CustomerBean customerBean = (CustomerBean) session.getAttribute("customerBean");
+
+        if(customerBean == null) {
+            res.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized: The user is not an admin");
+            return;
+        }
+
+        if(req.getParameter("assis-id") == null || req.getParameter("camp-id") == null) {
+            res.sendError(HttpServletResponse.SC_BAD_REQUEST, "Bad Request: Missing parameters");
+            return;
+        }
+        
+        // Estos parametros se pasaraian de la siguiente forma -> /summer_camp/completeInscription?camp-id=1&assis-id=2
+        int campId = Integer.parseInt(req.getParameter("camp-id"));
+        int assisId = Integer.parseInt(req.getParameter("assis-id"));
+
+        try {
+            // TODO: MOSTRAR AVISOS
+            Properties configProperties = new Properties();
+            Properties sqlProperties = new Properties();
+            sqlProperties.load(getServletContext().getResourceAsStream("/WEB-INF/sql.properties"));
+            configProperties.load(getServletContext().getResourceAsStream("/WEB-INF/config.properties"));
+
+            CampamentsManager campamentsManager = new CampamentsManager(sqlProperties, configProperties);
+            AssistantManager assistantManager = new AssistantManager(sqlProperties, configProperties);
+            InscriptionsManager inscriptionsManager = new InscriptionsManager(sqlProperties, configProperties);
+
+            CampamentDTO campamentDTO = campamentsManager.getById(campId);
+            AssistantDTO assistantDTO = assistantManager.getById(assisId);
+
+            inscriptionsManager.enrollParcial(campamentDTO, assistantDTO);
+            
+            res.setStatus(HttpServletResponse.SC_CREATED);
+        } catch (Exception e) {
+            e.getStackTrace();
+            res.sendError(HttpServletResponse.SC_BAD_REQUEST, "Bad Request: " + e.getMessage());
+        }
+    }
+    
     @Override
     protected void doDelete(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
         HttpSession session = req.getSession();
